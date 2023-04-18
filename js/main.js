733ddc3f50b65d22b71d80521bd251fd096fa51a
korkmazfb <32840@ma-web.nl>
2023-04-18 15:16:57 +0200

    zit vast door een bug in me code morgen verder werken

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ class Header{
         this.logoIElement = document.createElement("i");
         this.logoIElement.classList = "header__logo  fa-solid fa-face-smile-wink";
 
-        this.logoHeadingElement = document.createElement("h1");
+        this.logoHeadingElement = document.createElement("h2");
         this.logoHeadingElement.classList = "header__text";
         this.logoHeadingElement.innerText = "Collection of Happiness";
 
@@ -22,12 +22,81 @@ class Header{
 }
 
 
+class CollectionMain{
+    placeToBeRenderCollectionMain;
+    leftPanel;
+
+    constructor(placeToBeRenderCollectionMain){
+        this.placeToBeRenderCollectionMain = document.getElementsByTagName(placeToBeRenderCollectionMain)[0];
+
+        this.collectionMainElement = document.createElement("main");
+        this.collectionMainElement.classList = "collection";
+
+
+        //komt door deze regel
+        this.leftpanel = new LeftPanel(this.collectionMainElement);
+    }
+
+    render(){
+        this.placeToBeRenderCollectionMain.appendChild(this.collectionMainElement);
+        
+    }
+}
+
+
+class LeftPanel{
+    collectionMainElement;
+    constructor(collectionMainElement){
+        this.collectionMainElement = collectionMainElement;
+        this.lefsectionElement = document.createElement("section");
+        this.leftsectionElement.classList = "collection__section collection__section--left";
+
+    }
+
+    render(){
+        this.mainElement.appendChild(this.leftsectionElement);
+    }
+   
+
+}
+
+class Footer{
+    placeToBeRenderFooter;
+
+    constructor(placeToBeRenderFooter){
+        this.placeToBeRenderFooter = document.getElementsByTagName(placeToBeRenderFooter)[0];
+        this.footerElement = document.createElement("footer");
+        this.footerElement.classList = "footer"
+
+        this.footerTextElement = document.createElement("h3");
+        this.footerTextElement.classList = "footer__text";
+        this.footerTextElement.innerText = "Gemaakt door - Mert SD2C MediaCollege";
+
+
+
+    }
+
+    
+    render(){
+        this.placeToBeRenderFooter.appendChild(this.footerElement);
+        this.footerElement.appendChild(this.footerTextElement);
+    }
+  
+
+}
+
+
 
 class App{
     header;
+    mainElement;
+    footer;
 
     constructor(){
         this.header = new Header("body");
+        this.mainElement = new CollectionMain("body");
+        this.footer = new Footer("body");
+        
 
 
 
@@ -35,6 +104,8 @@ class App{
 
 
         this.header.render();
+        this.mainElement.render();
+        this.footer.render();
 
     }
 
